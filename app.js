24de794756d1513cbe146a1b61fc7ce316d66e9a
Rob McVey <mcvey.rob@gmail.com>
2023-11-14 00:58:19 -0800

    Updated default port

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
